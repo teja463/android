7a4d09f7aa74f0060b9ea64c922b75a951a9b869
teja463 <teja463@gmail.com>
2017-10-30 13:28:44 +0530

    Added intent demo app and updated just java with styles and intents

---
+++ IntentDemo/app/src/main/java/com/example/bponnuru/intentdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.bponnuru.intentdemo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        showMap(Uri.parse("geo:12.9, 77.5"));
+    }
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
--- JustJava/app/src/main/java/com/example/bponnuru/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/bponnuru/justjava/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.bponnuru.justjava;
 
 import java.text.NumberFormat;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -13,30 +18,59 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
     private static final int PRICE = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayPrice(quantity*PRICE);
+
+    }
+
+    private int calculatePrice() {
+        return quantity * PRICE;
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-//        display(quantity);
-        displayPrice(quantity*PRICE);
+        int price = calculatePrice();
+        CheckBox whipCream = (CheckBox) findViewById(R.id.whip_cream);
+        CheckBox choclate = (CheckBox) findViewById(R.id.chocklate);
+
+        EditText name = (EditText) findViewById(R.id.name);
+        String custName = name.getText().toString();
+        if (whipCream.isChecked()) {
+            price = price + ( quantity * 2 );
+        }
+        if (choclate.isChecked()) {
+            price = price + ( quantity * 3 );
+        }
+        String summary = getOrderSummary(custName, price);
+        displayPrice(summary);
+        sendMail(custName, summary);
     }
 
-    public void increment(View view){
+    private void sendMail(String name, String subject){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.thank_you, name));
+        intent.putExtra(Intent.EXTRA_TEXT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    public void increment(View view) {
         quantity++;
         display(quantity);
     }
 
-    public void decrement(View view){
-        quantity--;
+    public void decrement(View view) {
+        if (quantity > 0)
+            quantity--;
         display(quantity);
     }
+
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -45,8 +79,16 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(int number){
+
+    private void displayPrice(String summary) {
         TextView priceView = (TextView) findViewById(R.id.price_text_view);
-        priceView.setText(""+NumberFormat.getCurrencyInstance().format(number));
+        priceView.setText(summary);
+    }
+
+    public String getOrderSummary(String name, int price) {
+        String orderSummary = "";
+        orderSummary += getString(R.string.thank_you, name);
+        orderSummary += getString(R.string.you_need_to_pay) + NumberFormat.getCurrencyInstance().format(price);
+        return orderSummary;
     }
 }
